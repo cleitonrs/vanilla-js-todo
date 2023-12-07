dae6e237efbfba6f4dfc10a36b5d1d0128508027
cleitonrs <rscleitonrs@gmail.com>
2023-12-07 19:00:08 -0300

    adding todo

---
+++ app.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('.form-add-todo')
+const ul = document.querySelector('.todos-container')
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const todo = event.target.add.value.trim()
+  if (todo.length) {
+    ul.innerHTML += `
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </li>
+    `
+  }
+  event.target.reset()
+})
